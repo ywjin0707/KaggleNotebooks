c46cef0d2eb444d687b44d03586c03a1b50cf017
Yong Won Jin <yw_jin0707@hotmail.com>
2022-03-14 11:19:56 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import requests
 
 img_data = requests.get("https://capture-website-api.herokuapp.com/capture?url=https://www.kaggle.com/yongwonjin").content
-with open('kaggleprofile.jpg', 'wb') as handler:
+with open('./kaggleprofile.jpg', 'wb') as handler:
   handler.write(img_data)
