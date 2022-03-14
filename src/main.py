f3535c0b39fa47db24ed5fb7dccc0aeab59ad28f
Yong Won Jin <yw_jin0707@hotmail.com>
2022-03-14 11:26:56 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+from pathlib import Path
+import datetime
+import pytz
 import requests
 
+def update_footer():
+    timestamp = datetime.datetime.now(pytz.timezone("Canada/Winnipeg")).strftime("%c")
+    footer = Path('../FOOTER.md').read_text()
+    return footer.format(timestamp=timestamp)
+
 img_data = requests.get("https://capture-website-api.herokuapp.com/capture?url=https://www.kaggle.com/yongwonjin").content
-with open('./kaggleprofile.jpg', 'wb') as handler:
+with open(Path('./kaggleprofile.jpg'), 'wb') as handler:
   handler.write(img_data)
+
+readme = Path('../README.md').read_text()
+with open('../README.md', "w+") as f:
+    f.write(updated_readme + update_footer())
