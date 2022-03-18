e807c7632fe051cc2fefc03e9c994b202273cd98
Yong Won Jin <yw_jin0707@hotmail.com>
2022-03-18 23:31:33 +0900

    Update main.py to use Selenium
    Use Selenium to take screenshot instead

--- src/main.py
+++ src/main.py
@@ -1,17 +1,37 @@
 from pathlib import Path
 import datetime
 import pytz
-import requests
+# import requests
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 def update_footer():
     timestamp = datetime.datetime.now(pytz.timezone("Canada/Central")).strftime("%c")
     footer = Path('../FOOTER.md').read_text()
     return footer.format(timestamp=timestamp)
 
-img_data = requests.get("https://capture-website-api.herokuapp.com/capture?url=https://www.kaggle.com/yongwonjin").content
-with open(Path('./kaggleprofile.png'), 'wb') as handler:
-  handler.write(img_data)
+## retrieve screenshot from Kaggle using Selenium
+PATH = "/usr/bin/chromedriver"
+Xpath = '//*[@id="site-container"]/div/div[5]/div/div[2]/div'
+
+driver = webdriver.Chrome(PATH)
+driver.implicitly_wait(10)
+
+driver.get("https://kaggle.com/yongwonjin")
+wait = WebDriverWait(driver, 15)
+wait.until(EC.element_to_be_clickable((By.XPATH, Xpath)))
+driver.find_element_by_xpath(Xpath).click()
+driver.save_screenshot("./src/kaggleprofile.png")
+
+# img_data = requests.get("https://capture-website-api.herokuapp.com/capture?url=https://www.kaggle.com/yongwonjin").content
+# with open(Path('./kaggleprofile.png'), 'wb') as handler:
+#   handler.write(img_data)
 
 readme = Path('../README.md').read_text()
+readme_base = readme[:readme.find('<hr>')] # anchor position
+
 with open('../README.md', "w+") as f:
-    f.write(readme + update_footer())
+    f.write(readme_base + update_footer())
