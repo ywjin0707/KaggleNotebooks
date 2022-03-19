3620a3b2f30c17adff5d5470a335a869f5e17418
Yong Won Jin <yw_jin0707@hotmail.com>
2022-03-20 00:25:38 +0900

    Update main.py
    add chrome options for window size

--- src/main.py
+++ src/main.py
@@ -21,6 +21,7 @@ Xpath = '//*[@id="site-container"]/div/div[5]/div/div[2]/div'
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--single-process")
 chrome_options.add_argument("--disable-dev-shm-usage")
